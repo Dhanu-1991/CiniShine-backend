4ef8d9704b21a765969a65a21f5d385172368c8d
Dhanush Kumar <dhanushkumarvr019@gmail.com>
2025-07-22 16:41:52 +0530

    Fix: defined router before using it

--- routes/paymentRoutes/cashfree.js
+++ routes/paymentRoutes/cashfree.js
@@ -2,6 +2,7 @@ import express from "express";
 import payment from "../../controllers/payment-gateway-controllers/payment.js";
 import paymentVerify from "../../controllers/payment-gateway-controllers/payment-verify.js";
 import {handleCashfreeWebhook} from "../../controllers/payment-gateway-controllers/payment-webhook.js";
+const router = express.Router();
 router.post("/payment", payment);
 router.post("/payment-verify", paymentVerify);
 router.post("/payment-webhook", express.raw({ type: 'application/json' }), handleCashfreeWebhook); // ✅ proper webhook handler
