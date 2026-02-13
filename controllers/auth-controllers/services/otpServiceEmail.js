1a7293f9f7130e09ff2a08abac9eb59d3695fe64
Dhanush Kumar <dhanushkumarvr@gmail.com>
2026-02-13 20:39:43 +0530

    changed OTP settings6

--- controllers/auth-controllers/services/otpServiceEmail.js
+++ controllers/auth-controllers/services/otpServiceEmail.js
@@ -5,13 +5,17 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false, // IMPORTANT
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 export async function sendOtpToEmail(to, otp) {
   try {
